edfe368287591b9deaf3fe3058de41a3844e5cb2
Fernando Eckstein <fernando_eckstein@hotmail.com>
2020-09-25 01:15:07 -0300

    Initial commit

---
+++ Hiper.Products/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hiper.Products.Models
+{
+    public class Product
+    {
+    }
+}
